dd60c85359435a473ab8d43e630a946a505f99db
Aibek Ryskaliyev <aryskaliyev@nu.edu.kz>
2020-12-17 20:26:33 +0000

    Intro to Threading

--- threading/img-downloader.py
+++ threading/img-downloader.py
@@ -1,9 +1,10 @@
 import concurrent.futures
 import requests
 import time
+import os
 
 img_urls = [
-'https://images.unsplash.com/photo-1516117172878-fd2c41f4a759',
+    'https://images.unsplash.com/photo-1516117172878-fd2c41f4a759',
     'https://images.unsplash.com/photo-1532009324734-20a7a5813719',
     'https://images.unsplash.com/photo-1524429656589-6633a470097c',
     'https://images.unsplash.com/photo-1530224264768-7ff8c1789d79',
@@ -21,6 +22,9 @@ img_urls = [
     ]
 
 t1 = time.perf_counter()
+
+destination = 'img'
+os.chdir(destination)
 
 def download_image(img_url):
     img_bytes = requests.get(img_url).content
